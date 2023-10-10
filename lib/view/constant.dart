28224fe58e018623659637968e5dc8657aaaefaf
unSerori <pd.ai.374005475@gmail.com>
2023-10-10 10:22:36 +0900

    [update]constance.dartにwsクラス作れないかなーの検証

--- lib/view/constant.dart
+++ lib/view/constant.dart
@@ -3,6 +3,10 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import '../view/pages/page_massages.dart';
 import './items.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+import 'package:http/http.dart' as http;  // http
+import 'dart:convert';  // json
+
 
 class Constant {
   static const Color main = Color(0xFF00849C);
@@ -227,6 +231,9 @@ void addMessage(String messenger, bool messagebool, String message, bool StampBo
     'index': index,
     'whose': whose,
   };
+
+  // 
+  
   items.message['sender'][messenger].add(newMessage);
 }
 
@@ -258,4 +265,154 @@ bool roomIDcheck(String roomID) {
     }
   }
   return false;
+}
+
+class Url {
+  // URLとかポートとかプロトコルとか
+  static const String serverIP = "10.0.2.2";  // "127.0.0.1""10.200.0.82""tidalhip.local""10.200.0.115"10.25.10.10710.200.0.163
+  static const String server_port = "5000";
+  static const String protocol = "http";
+  static baseUrl() {  // 鯖のURLを設定
+    return protocol + "://" + serverIP + ":" + server_port;
+  }
+  static baseUrlWithoutProtcol() {  // 鯖のURLを設定
+    return "://" + serverIP + ":" + server_port;
+  }
+
+}
+
+// Httpリクエストを投げるクラス
+// 
+class HttpToServer {  // HttpLib
+  // インスタンス変数
+
+  // コンストラクタってなに？？
+  HttpToServer();
+
+  // "URLパラメータ", "HTTPメソッド", "body"  例えば (/send_userInfo", "POST", items.userInfo)
+  // HTTP リクエストを送信する関数 
+  static Future<List> httpReq(String path_para, String method, Map<String, dynamic> body) async {
+
+    
+    //header
+    var headersList = {
+      'Accept': '*/*',
+      'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
+      'Content-Type': 'application/json',
+    };
+    // "Access-Control-Allow-Origin": "*"
+
+    // リクエスト作成
+    var req = http.Request("POST", Uri.parse(Url.baseUrl() + path_para));  // HTTPリクエストメソッドの種類とuriから
+      // debugPrint(req.toString());
+    req.headers.addAll(headersList);  // header情報を追加
+      // debugPrint(req.toString());
+    req.body = json.encode(body);  // bodyをjson形式に変換
+      // debugPrint(req.toString());
+
+    try {
+
+      // HTTPリクエストを送信。 seconds: 5 で指定した秒数応答がなかったらタイムアウトで例外を発生させる
+      var res = await req.send();//.timeout(const Duration(seconds: 5));
+      debugPrint(res.toString());
+      // レスポンスをストリームから文字列に変換して保存
+      final resBody = await res.stream.bytesToString();
+
+      // ステータスコードが正常ならtrueと内容を返す
+      if (res.statusCode >= 200 && res.statusCode < 300) {
+        debugPrint([true, resBody].toString());
+        return [true, resBody];
+      } else {
+        debugPrint([false, res.statusCode.toString(), resBody].toString());
+        return [false, res.statusCode.toString(), resBody];
+      }
+
+    } catch (e) {  // タイムアウトしたとき。
+
+      debugPrint("exception error: " + e.toString());
+      debugPrint([false, "おうとうないよ；；"].toString());
+      return [false, "おうとうないよ；；"];
+
+    }
+
+
+    // if (response.statusCode == 200) {
+    //   // レスポンスが成功した場合
+    //   final data = json.decode(response.body);
+      
+    // } else {
+    //   // レスポンスがエラーの場合
+     
+    // }
+  }
+}
+class WS extends StatefulWidget {
+  const WS({super.key});
+
+  @override
+  State<WS> createState() => _WSState();
+}
+
+class _WSState extends State<WS> {
+  // 
+  // パッケージからインスタンス
+  late io.Socket socket;
+
+  // wsを初期化
+  @override
+  void initState() {
+    super.initState();
+    var req = http.Request("POST", Uri.parse("http" + Url.baseUrlWithoutProtcol() + "/post_"));  // HTTPリクエストメソッドの種類とuriから
+    try {
+    req.body = json.encode({"key": "testhoge"});  // bodyをjson形式に変換
+      var res = req.send().then((value) => debugPrint(value.statusCode.toString()));//.timeout(const Duration(seconds: 5));
+    } catch (e) {
+      
+    }
+
+    // 接続先Socket.IOサーバーのURLと通信方式を設定
+    socket = io.io("http" + Url.baseUrlWithoutProtcol(), <String, dynamic>{
+      'transports': ['websocket'],
+    });
+    // 接続
+    socket.connect();
+
+    // イベント
+    socket.on('response', (data) {
+      setState(() {
+        debugPrint(data);
+      });
+    });
+
+  }
+
+  void _sendMessage(String msg) {
+    String message = msg;
+    if (message.isNotEmpty) {  // からでなければ
+      socket.emit('message', message);
+    }
+  }
+
+  // ログアウト、アプリケーションのバックグラウンド実行時、または接続が不要になったとき
+  @override
+  void dispose() {
+    socket.disconnect();
+    super.dispose();
+  }
+
+/* 
+// 反映するとき   
+            Container(
+              margin:EdgeInsets.all(_screenSizeWidth*0.02),
+              height: _screenSizeHeight*0.075,
+              alignment: Alignment(0, 0),
+              decoration: BoxDecoration(color: Constant.white,borderRadius: BorderRadius.circular(16)),
+              child: CustomText(text: testText, fontSize: _screenSizeWidth*0.05, color: Constant.blackGlay),
+            )
+ */
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
